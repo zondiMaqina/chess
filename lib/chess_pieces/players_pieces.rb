9fa78b0b4487616b5cdf57e61ec0fdb2dfb463b8
zondiMaqina <zondimaqina0@gmail.com>
2025-02-18 17:02:13 +0200

    add hash to identify pieces seperately

--- lib/chess_pieces/players_pieces.rb
+++ lib/chess_pieces/players_pieces.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 
-# class for both players' pieces
+# class for storing both players' pieces
 class PlayersPieces
   def initialize
     @black_pieces = [%w[♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟], %w[♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜]]
     @white_pieces = [%w[♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙], %w[♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖]]
+    @identifiers = {pawn: %w[♟ ♙], rook: %w[♜ ♖], knight: %w[♞ ♘], bishop: %w[♝ ♗], queen: %w[♛ ♕], king: %w[♚ ♔]}
   end
 end
